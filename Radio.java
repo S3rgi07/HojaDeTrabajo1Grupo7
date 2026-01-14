d7e5210a9ee4ce5d7e4084625f7b8989eeef548a
JordiCardona7 <jordicardona1@gmail.com>
2026-01-14 13:03:38 -0600

    contratos interfaz 2

---
+++ Radio.java
@@ -0,0 +1,11 @@
+public interface Radio {
+    void forwardFrequency(double frequency);
+    void backwardFrequency(double frequency);
+    void changeStation(double station);
+    void turnOnOff();
+    void saveFrequency(double frequency, int slot);
+    void loadFrequency(int slot);
+
+
+    
+}
