41d113a6c7fa94064512116f348a0e7d9676cadf
JordiCardona7 <jordicardona1@gmail.com>
2026-01-19 17:21:58 -0600

    clase radio

---
+++ CarRadio.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class CarRadio {
+    private boolean isOn = false;
+    private String currentStation;
+    private int currentFrecuencyAm;
+    private double currentFrecuencyFm;
+    private boolean isAm;
+    private ArrayList<Slots> slotsFavorites = new ArrayList<>()
+
+    
+}
