50498f47a6e8d7576b5ba33e32cd79290ed0d176
JordiCardona7 <jordicardona1@gmail.com>
2026-01-19 18:31:57 -0600

    primero 3 metodos CarRadio

--- CarRadio.java
+++ CarRadio.java
@@ -3,10 +3,81 @@ import java.util.ArrayList;
 public class CarRadio {
     private boolean isOn = false;
     private String currentStation;
-    private int currentFrecuencyAm;
-    private double currentFrecuencyFm;
+    private double currentFrecuency;
+    private double lastFrecuency = 530;
     private boolean isAm;
-    private ArrayList<Slots> slotsFavorites = new ArrayList<>()
+    private double[] slotsFavorites = new double[12];
+
+
+
+    public void changeStation(String currentStation){
+        if (isAm == true) {
+            isAm = false;
+            double holder = lastFrecuency;
+            lastFrecuency = currentFrecuency;
+            currentFrecuency = holder;
+        }
+        else {
+            isAm = true;
+            double holder = lastFrecuency;
+            lastFrecuency = currentFrecuency;
+            currentFrecuency = holdader;
+        }
+
+
+    }
+
+    public void forwardFrequency(double currentFrecuency){
+        if (isAm == false) {
+            if (currentFrecuency < 107.9) {
+                currentFrecuency += 0.2;
+            }
+
+            else{
+                currentFrecuency = 87.9;
+            }
+        
+            
+        }
+        else if (isAm == true) {
+            if (currentFrecuency < 1610) {
+                currentFrecuency += 10;
+            }
+
+            else{
+                currentFrecuency = 530;
+            }
+        }
+
+    };
+
+    public void backwardFrequency(double currentFrecuency){
+        if (isAm == false) {
+            if (currentFrecuency > 87.9) {
+                currentFrecuency -= 0.2;
+            }
+
+            else{
+                currentFrecuency = 107.9;
+            }
+        
+            
+        }
+        else if (isAm == true) {
+            if (currentFrecuency > 530) {
+                currentFrecuency -= 10;
+            }
+
+            else{
+                currentFrecuency = 1610;
+            }
+        }
+    };
+
+
+
+
+    
 
     
 }
